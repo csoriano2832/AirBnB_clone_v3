ed78fecb90d6e025225af1cf5abaaba066759428
csoriano2832 <2832@holbertonschool.com>
2021-09-21 14:47:30 +0000

    Revert "Fix responses"
    This reverts commit 81b56061e5be7f8aac7550a00de922d2f7723a2a.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,7 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-from api.v1.views.states import *
+import api.v1.views.states
 import api.v1.views.cities
 import api.v1.views.amenities
 import api.v1.views.users
-import api.v1.views.places
