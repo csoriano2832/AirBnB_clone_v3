6c6a693154e2f2c15fb64a40bd3840dee7b78920
csoriano2832 <2832@holbertonschool.com>
2021-09-21 14:27:21 +0000

    reverting changes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """ This module handles the HTTP methods of a state object"""
-from flask import jsonify, abort, request, Response
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+all_states = storage.all('State')
+states = []
+
+for state in all_states.values():
+    states.append(state.to_dict())
+
 
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
-def states():
+def get_states():
     """ Handles HTTP request of all the state object """
 
     if request.method == 'POST':
         data = request.get_json()
         if not data:
-            return Response('Not a JSON', status=400)
+            abort(400, 'Not a JSON')
         if 'name' not in data:
-            return Response('Missing name', status=400)
+            abort(400, 'Missing name')
         state = State(name=data.get('name'))
         state.save()
         return jsonify(state.to_dict()), 201
 
-    all_states = storage.all('State')
-    states = []
-
-    for state in all_states.values():
-        states.append(state.to_dict())
     return jsonify(states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def state(state_id=None):
+def get_state(state_id=None):
     """ Handles HTTP requests of a single state object """
     state = storage.get(State, state_id)
     if state is None:
@@ -44,7 +45,7 @@ def state(state_id=None):
     if request.method == 'PUT':
         data = request.get_json()
         if not data:
-            return Response('Not a JSON', 400)
+            abort(400, 'Not a JSON')
         data['id'] = state.id
         data['created_at'] = state.created_at
         state.__init__(**data)
